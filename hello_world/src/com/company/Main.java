60682c746caaf27b5ccbe74dbec576a53783a5d0
rantion <Rachel@Rachels-MacBook-Pro.local>
2013-11-14 19:35:14 -0700

    hello world
    test 2

--- hello_world/src/com/company/Main.java
+++ hello_world/src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Is it me you're looking for?");
+        System.out.println("I can see it in your eyes");
+        System.out.println("I can see it in your smiiiile");
         // write your code here
     }
 }
